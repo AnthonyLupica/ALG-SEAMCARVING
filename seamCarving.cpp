7ea33845f7d5744bcce7e0f929ea8cc0ad5b27ad
Anthony R. Lupica <arl127@uakron.edu>
2023-04-16 21:14:14 -0400

    SeamCarver appears to work!

--- seamCarving.cpp
+++ seamCarving.cpp
@@ -1,5 +1,21 @@
 /* 
     seamCarving.cpp
+
+    Seam carving changes the size of an image by removing the least visible pixels in the image. 
+    The visibility of a pixel can be defined using an energy function. Seam carving can be done by finding a 
+    one-pixel wide path of lowest energy crossing the image from top to bottom (vertical path) or 
+    from left to right (horizontal path) and removing the path (seam).
+
+    Assumptions: 
+        The pgm file provided adheres to the following format...
+        
+        P2                       ; P2 designating greyscale image 
+        # Created by IrfanView   ; optional comment
+        y x                      ; columns(y) by rows(x)
+        255                      ; upper bound on values
+        *                        ; pixel data begins here 
+        *
+        * 
 */
 
 #include <iostream> 
@@ -8,6 +24,8 @@
 #include <string> 
 #include <sstream>
 #include <cmath> 
+#include <algorithm>
+#include <utility> 
 
 using std::cout;
 using std::cerr;
@@ -19,9 +37,10 @@ using std::string;
 using std::stringstream;
 
 vector<vector<int>> initImageMap(const string &filename);
-vector<vector<int>> initEnergyMap(const vector<vector<int>> imageMap);
-vector<vector<int>> initCumulativeEnergyMap(const vector<vector<int>> energyMap);
-void displayMap(const vector<vector<int>> map);
+vector<vector<int>> initEnergyMap(const vector<vector<int>> &imageMap);
+vector<vector<int>> initCumulativeEnergyMap(const vector<vector<int>> &energyMap);
+void seamCarver(vector<vector<int>> &imageMap, const vector<vector<int>> &cumulativeEnergyMap);
+void displayMap(const vector<vector<int>> &map);
 
 int main(int argc, char* argv[]) 
 {
@@ -46,18 +65,27 @@ int main(int argc, char* argv[])
     // INITIALIZE THE ENERGY MAP
     vector<vector<int>> E = initEnergyMap(I);
     
-    cout << "\nDerived Energy Matrix: \n";
+    cout << "\nEnergy Matrix: \n";
     displayMap(E);
 
     // INITIALIZE THE CUMULATIVE ENERGY MAP 
-    //vector<vector<int>> CE = initCumulativeEnergyMap(E);
+    vector<vector<int>> CE = initCumulativeEnergyMap(E);
+
+    cout << "\nCumulative Energy Matrix: \n";
+    displayMap(CE);
+
+    // CARVE OUT A SEAM
+    seamCarver(I, CE); 
+
+    cout << "\nSEAM CARVED IMAGE: \n";
+    displayMap(I);
    
     return 0;
 }
 
-// <Summary> A vector<vector<int>> is populated with the image values contained in the pgm file, 'filename' </Summary> 
-// <Param name='filename'> name of a file with .pgm extension </Param> 
-// <Return> The vector<vector<int>> by value </Return> 
+// <Summary> a 2D vector of integers is populated with the image pixel values comprising the pgm image file, 'filename' </Summary> 
+// <Param name='filename'> name of a file with a .pgm extension </Param> 
+// <Return> the resultant image map by value </Return> 
 // <Assumptions> 
 //      initImageMap assumes the pgm file format outlined in the project description is rigorously adhered to. 
 //      noteably, a hard assumption is made that one optional comment is in the file, necessarily on line two (if it exists)
@@ -74,7 +102,7 @@ vector<vector<int>> initImageMap(const string &filename)
         exit(1);
     }
 
-    // #region parse_to_data 
+    // #REGION parse_to_data 
     string temp_line;
     
     // handle file format line
@@ -87,7 +115,7 @@ vector<vector<int>> initImageMap(const string &filename)
         exit(1);
     }
 
-    // @Note - crucially, the code here assumes line two of the header is the singular comment-line, or there are no comments in the file at all
+    // @NOTE - crucially, the code here assumes line two of the header is the singular comment-line, or there are no comments in the file at all
     if (pgmInputFile.peek() == '#') 
     { 
         getline(pgmInputFile, temp_line); // skip optional comment
@@ -111,9 +139,9 @@ vector<vector<int>> initImageMap(const string &filename)
     }
 
     getline(pgmInputFile, temp_line); // maximum greyscale value
-    // #endregion
+    // #ENDREGION
 
-    // #region parse_data
+    // #REGION parse_data
     // read raw pixel data into a string, and subsequently into a stringstream
     string pixelData;
     stringstream ssPixelData;
@@ -138,66 +166,218 @@ vector<vector<int>> initImageMap(const string &filename)
     int pixel = 0;
     for (int i = 0; i < rows; ++i)
     {
-        vector<int> temp;
+        // outer-for iterates over rows
+
+        vector<int> rowResult;
         for (int j = 0; j < columns; ++j)
         {
+            // inner-for iterates over individual pixels in each row
             ssPixelData >> pixel;
             
-            temp.push_back(pixel);
+            rowResult.push_back(pixel);
         }
         
-        result.push_back(temp);
+        result.push_back(rowResult);
     }
-    // #endregion
+    // #ENDREGION
 
     return result;
 }
 
-// <Summary> Initialize an energy map given an image map produced by initImageMap </Summary> 
-// <Param name='imageMap'> A 2D vector containing the pixel data of a pgm file </Param> 
-// <Return> The energy map by value as a vector<vector<int>> </Return> 
-vector<vector<int>> initEnergyMap(const vector<vector<int>> imageMap)
+// <Summary> a 2D vector of integers is populated with pixel energy values given an image map produced by initImageMap </Summary> 
+// <Param name='imageMap'> a 2D vector containing the pixel data of a pgm file </Param> 
+// <Return> the resultant energy map by value </Return> 
+vector<vector<int>> initEnergyMap(const vector<vector<int>> &imageMap)
 {
     vector<vector<int>> result;
     for (int i = 0; i < imageMap.size(); ++i)
     {
         // outer-for iterates over rows
 
-        vector<int> temp;
+        vector<int> rowResult;
         for (int j = 0; j < imageMap[i].size(); ++j) 
         {
             // inner-for iterates over individual pixels in each row
             
-            // #region find ΔI along X axis 
+            // #REGION find ΔI along X axis 
             // bounds checking X
             int left = (j - 1) >= 0 ? imageMap[i][j - 1] : imageMap[i][j]; 
             int right = (j + 1) < imageMap[i].size() ? imageMap[i][j + 1] : imageMap[i][j];
 
             int changeX = abs(imageMap[i][j] - left) + abs(imageMap[i][j] - right);
-            // #endregion
+            // #ENDREGION
 
-            // #region find ΔI along Y axis 
+            // #REGION find ΔI along Y axis 
             // bounds checking Y
             int up = (i - 1) >= 0 ? imageMap[i - 1][j] : imageMap[i][j];
             int down = (i + 1) < imageMap.size() ? imageMap[i + 1][j] : imageMap[i][j];
 
             int changeY = abs(imageMap[i][j] - up) + abs(imageMap[i][j] - down);
-            // #endregion
+            // #ENDREGION
             
             // store the energy of the pixel
-            temp.push_back(changeX + changeY);
+            rowResult.push_back(changeX + changeY);
         }
 
-        result.push_back(temp);
+        result.push_back(rowResult);
     }
     
     return result;
 }
 
-// <Summary> Display a 2D vector </Summary> 
-// <Param name='map'> A 2D vector to be displayed </Param> 
+// <Summary>
+//      a 2D vector of integers is populated with cumulative energy values to provide the seam 
+//      carving algorithm with information about the lowest energy seam 
+// </Summary> 
+// <Param name='energyMap'> the energy map which is used to derive the CE map </Param> 
+// <Return> 
+//      the resultant cumulative energy map by value
+//      the lowest energy value in the final row of the CE matrix represents the pixel 
+//      which ends the lowest energy seam
+// </Return> 
+vector<vector<int>> initCumulativeEnergyMap(const vector<vector<int>> &energyMap)
+{
+    // initialize result with the contents of the energyMap
+    vector<vector<int>> result (energyMap);
+
+    /*  objective: for each pixel, act as though this is the end of the seam.
+                   write to result at this indexed pixel, the "cumulative energy"
+                   of itself and the pixel it would have come from. 
+        EX: X comes from 1 (the lowest energy ancestor)
+        +---+---+---+---+
+        | 1 | 2 | 3 | 4 |
+        +---+---+---+---+
+        |   | X |   |   |
+        +---+---+---+---+
+
+        from the perspective of X, we are looking at ... 
+            energyMap[i - 1][j - 1], energyMap[i - 1][j], and energyMap[i - 1][j + 1]
+        
+        bounds checking will need to be done on j
+    */
+
+    for (int i = 1; i < result.size(); ++i)
+    {
+        // outer-for iterates over rows (begin iterating on second row)
+
+        for (int j = 0; j < result[i].size(); ++j) 
+        {
+            // inner-for iterates over individual pixels in each row
+            
+            //#REGION bounds checking
+            // create a list of up to 3 (or 2) seam origin pixels in the row above that can be connected
+            // to the seam pixel in the current row.
+            vector<int> validSeamOriginList;
+
+            // validate bounds of upper-left
+            if ((j - 1) >= 0) 
+            { 
+                validSeamOriginList.push_back(result[i - 1][j - 1]);
+            }
+
+            // no need to validate upper
+            validSeamOriginList.push_back(result[i - 1][j]);
+
+            // validate bounds of upper-right
+            if ((j + 1) < energyMap[i].size()) 
+            { 
+                validSeamOriginList.push_back(result[i - 1][j + 1]);
+            }
+            //#ENDREGION
+
+            // at this pixel, add to its energy the minimum energy contained within validSeamOriginList
+            result[i][j] += *std::min_element(validSeamOriginList.begin(), validSeamOriginList.end());
+        }
+    }
+
+    return result;
+}
+
+// <Summary>
+//      given the image map and its cumulative energy map, "carve out" the lowest energy seam 
+//      from the image map
+// </Summary> 
+// <Param name='imageMap'> the image map to be modified by the seamCarver </Param> 
+// <Param name='cumulativeEnergyMap'> the CE map to be traced back to determine the lowest energy seam </Param> 
+// <Return> N/A -> the seam-carved image map is modified in-place </Return> 
+void seamCarver(vector<vector<int>> &imageMap, const vector<vector<int>> &cumulativeEnergyMap)
+{
+    // modify imageMap by identifying the pixel in each row that is an element of the 
+    // lowest energy seam, shifting it the end of the row, and popping back that element
+
+    // each element of seam_column_indices corresponds to a row in the image map, 
+    // and contains the column index of the seam pixel in that row.
+    int seam_column_indices[cumulativeEnergyMap.size()] = { -1 }; 
+
+    // get the index of the seam-ending pixel and put at the end of the seam pixel list
+    // the seam-ending pixel is the element in the final row of the cumulativeEnergyMap with the lowest energy
+    int num_rows = cumulativeEnergyMap.size();                                                                                
+    auto seam_end_itr = std::min_element(cumulativeEnergyMap[num_rows - 1].begin(), cumulativeEnergyMap[num_rows - 1].end()); 
+    int seam_end_index = std::distance(cumulativeEnergyMap[num_rows - 1].begin(), seam_end_itr);                              
+    seam_column_indices[num_rows - 1] = seam_end_index;
+
+    // iterate in reverse-row order, descending the seam
+    // for each iteration, first remove the seam pixel for that row 
+    // and next trace-back the seam to find the connecting seam pixel below it for the next iteration
+    for (int i = cumulativeEnergyMap.size() - 1; i >= 0; --i)
+    {
+        //#REGION remove the seam_pixel for this row
+        // Remove the seam pixel from the current row by swapping it with adjacent pixels
+        // until it reaches the end of the row, and then "snipping" it off from the end.
+        int seam_pixel_index = seam_column_indices[i];
+        while (seam_pixel_index + 1 < cumulativeEnergyMap[i].size())
+        {
+            // while-loop swaps the seam pixel until it is at the end of the row 
+            std::swap(imageMap[i][seam_pixel_index], imageMap[i][seam_pixel_index + 1]);
+            ++seam_pixel_index;
+        }
+        imageMap[i].pop_back();
+        //#ENDREGION
+        
+        //#REGION find out what the next seam pixel index is
+        if ((i - 1) >= 0)
+        {
+            // if-block only reached if trace-back step doesn't take us out of bounds
+
+            //#REGION bounds checking
+            // create a list of 3 (or 2) seam origin pixels in the row above that can be connected
+            // to the seam pixel in the current row.
+            vector<int> validSeamOriginList; 
+
+            // validate bounds of upper-left
+            if ((seam_column_indices[i] - 1) >= 0) 
+            { 
+                validSeamOriginList.push_back(cumulativeEnergyMap[i - 1][seam_column_indices[i] - 1]);
+            }
+
+            // no need to validate upper
+            validSeamOriginList.push_back(cumulativeEnergyMap[i - 1][seam_column_indices[i]]);
+
+            // validate bounds of upper-right
+            if ((seam_column_indices[i] + 1) < cumulativeEnergyMap[i].size()) 
+            { 
+                validSeamOriginList.push_back(cumulativeEnergyMap[i - 1][seam_column_indices[i] + 1]);
+            }
+            //#ENDREGION
+
+            // among the possible seam trace-back pixels in the list, find the lowest energy
+            int seam_traceback_value = *std::min_element(validSeamOriginList.begin(), validSeamOriginList.end()); 
+            
+            // find the index in the trace-back row of this "lowest energy" pixel
+            auto seam_traceback_itr = std::find(cumulativeEnergyMap[i - 1].begin(), cumulativeEnergyMap[i - 1].end(), seam_traceback_value);
+            int seam_traceback_index = std::distance(cumulativeEnergyMap[i - 1].begin(), seam_traceback_itr);
+
+            // finally, assign this index as the next iterations seam pixel index
+            seam_column_indices[i - 1] = seam_traceback_index;
+        //#ENDREGION
+        }
+    }
+}
+
+// <Summary> display a 2D vector </Summary> 
+// <Param name='map'> a 2D vector to be displayed </Param> 
 // <Return> N/A </Return> 
-void displayMap(const vector<vector<int>> map)
+void displayMap(const vector<vector<int>> &map)
 {
     for (vector<int> row : map)
     {
